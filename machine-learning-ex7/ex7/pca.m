0533b3dd3cfe0cb6b69afefc308b3be5d640afc8
Ralph Bodenner <ralph@newrelic.com>
2016-05-26 09:54:46 -0700

    PCA

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -1,6 +1,6 @@
 function [U, S] = pca(X)
 %PCA Run principal component analysis on the dataset X
-%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   [U, S] = pca(X) computes eigenvectors of the covariance matrix of X
 %   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
 %
 
@@ -20,11 +20,9 @@ S = zeros(n);
 %       number of examples).
 %
 
+Sigma = (1/m) * X' * X;
 
-
-
-
-
+[U,S,V] = svd(Sigma);
 
 % =========================================================================
 
